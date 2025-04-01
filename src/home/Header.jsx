169c1f87294522ee29e16c1f50a3951a2c59c826
matheusressu <matheus.ressurreicao@sptech.school>
2025-04-01 01:01:24 -0300

    Header, Banner e Services

---
+++ src/home/Header.jsx
@@ -0,0 +1,30 @@
+import Logo from './components/Logo'
+
+import './Home.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="content-header">
+        <div className='logo'>
+          <a href="URL">
+            <Logo />
+          </a>
+        </div>
+        <div className='links'>
+          <a href="URL">Serviços</a>
+          <a href="URL">Sobre Nós</a>
+          <a href="URL">Equipe</a>
+          <a href="URL">Contato</a>
+        </div>
+        <div className='buttons'>
+          <button className='button-primary'>Cadastre-se</button>
+          <button className='button-secondary'>Login</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import Home from './home/Home'
+import Header from './home/Header'
+import Banner from './home/Banner'
+import Services from './home/Services'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home/>
+    <body>
+      <Header />
+      <Banner />
+      <Services />
+    </body>
   </StrictMode>
 
 )
