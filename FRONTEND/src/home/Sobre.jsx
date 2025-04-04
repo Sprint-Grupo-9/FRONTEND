bf259b41dea85924797f8e0a1ca6fc7e59bc874a
Yasmin Pereira <yasmin.caetano@sptech.school>
2025-04-04 15:13:28 -0300

    Ajustando a imagem

--- FRONTEND/src/home/Sobre.jsx
+++ FRONTEND/src/home/Sobre.jsx
@@ -1,26 +1,6 @@
 import './Home.css'
 import React from 'react';
-
-// const Sobre = () => {
-//   return (
-//     <div className="tab-about">
-//       <div className="about-content-wrapper">
-//         <div className="about-content">
-//           <h2 className="about-title">
-//             <span className="text-wrapper-12">Sobre Nós</span>
-//           </h2>
-//           <p className="about-description">
-//             Somos especializados em cuidados e transporte para seu pet.
-//           </p>
-//           <p className="about-address">Endereço: Rua Exemplo, 123 - Cidade</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Sobre;
-
+import Card from './components/CardSobre'
 
 function Sobre() {
 
@@ -41,7 +21,11 @@ function Sobre() {
             <span class="text-endereco">Rua Columbia 123, Vila Boa<br />São Paulo, SP </span>
           </p>
 
-          <div className='about-image'><img className='mask-group' src="{imageLinkPet}" alt="" /></div>
+          <div className='card-sobre'>
+            <Card link={"src/assets/mask-group-5.png"} />
+          </div>
+
+          {/* <div className='about-image'><img className='mask-group' src="{imageLinkPet}" alt="" /></div> */}
 
           <div className='about-title'>
             <div className='text-sobre'><h3>Sobre Nós</h3></div>
