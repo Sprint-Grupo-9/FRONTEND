b8afa2ccbbc8c92bc28b4d56aeee2e07401c5c35
Yasmin Pereira <yasmin.caetano@sptech.school>
2025-04-04 11:01:53 -0300

    Ajustes no sobre

--- FRONTEND/src/home/Sobre.jsx
+++ FRONTEND/src/home/Sobre.jsx
@@ -1,33 +1,59 @@
 import './Home.css'
-import Card from './components/Card'
-import imageLinkFundo from '../../../FRONTEND/src/assets/tab-about.png'
-import imageLinkPet from '../../../FRONTEND/src/assets/tab-about.png'
-import imageLinkPata from '../../../FRONTEND/src/assets/tab-about.png'
-function Sobre(){
+import React from 'react';
 
-    <img src="{imageLinkFundo}" alt="tab-about" />
+// const Sobre = () => {
+//   return (
+//     <div className="tab-about">
+//       <div className="about-content-wrapper">
+//         <div className="about-content">
+//           <h2 className="about-title">
+//             <span className="text-wrapper-12">Sobre Nós</span>
+//           </h2>
+//           <p className="about-description">
+//             Somos especializados em cuidados e transporte para seu pet.
+//           </p>
+//           <p className="about-address">Endereço: Rua Exemplo, 123 - Cidade</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-    return (
+// export default Sobre;
+
+
+function Sobre() {
+
+  return (
+
+    <section className='sobre-container'>
+
+      {/* <div className='tab-about'> */}
 
         <div className='about-content'>
-            <p className='descricao'>
-                text
-            </p>
 
-            <p className='endereco'>
+          <p className='descricao'>
+            text
+          </p>
+
+          <p className='endereco'>
             <span class="text-endereco">Endereço: <br /></span>
             <span class="text-endereco">Rua Columbia 123, Vila Boa<br />São Paulo, SP </span>
-            </p>
+          </p>
 
-            <div className='about-image'><img className='mask-group' src="{imageLinkPet}" alt="" /></div>
+          <div className='about-image'><img className='mask-group' src="{imageLinkPet}" alt="" /></div>
 
-            <div className='about-title'>
-                <div className='text-sobre'>Sobre Nós</div>
-                <img className='pata-sobre' src="{imageLinkPata}" alt="" />
-            </div>
-            
+          <div className='about-title'>
+            <div className='text-sobre'><h3>Sobre Nós</h3></div>
+            <img className='pata-sobre' src="{imageLinkPata}" alt="" />
+          </div>
+
         </div>
-    )
+      {/* </div> */}
+    </section>
+  )
 
-   
+
 }
+
+export default Sobre;
--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import Header from './home/Header'
 import Banner from './home/Banner'
 import Services from './home/Services'
+import Sobre from './home/Sobre'
 
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Header />
       <Banner />
       <Services />
+      <Sobre />
     </body>
   </StrictMode>
 
