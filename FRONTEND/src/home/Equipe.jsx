9b5b1f1b1d3317937af33ab095f3bb7f678f301b
Yasmin Pereira <yasmin.caetano@sptech.school>
2025-04-06 15:30:55 -0300

    equipe

--- FRONTEND/src/home/Equipe.jsx
+++ FRONTEND/src/home/Equipe.jsx
@@ -6,17 +6,37 @@ function Equipe() {
 
     return (
         <section className='time-container'>
-            <div className="time">
-
-                <div className="time-titulo">
-                    <h3 className='text-time'>Equipe</h3>
+            <div className="team-content">
+                <div className="overlap-9">
+                    <div className="text-wrapper-13">Dora</div>
+                    <div className="mask-group-wrapper"><div className='img-equipe-1'></div></div>
                 </div>
-                <div className="cards-equipe">
-                    <CardTime texto="Banho e Tosa" link="src/assets/banho-logo.svg" />
-                    <CardTime texto="Taxi Dog" link="src/assets/taxi-logo.svg" />
-                    <CardTime texto="Veterinário" link="src/assets/pet-logo.svg" />
+                <div class="overlap-10">
+                    <div className="text-wrapper-14">Lucas</div>
+                    <div className="mask-group-wrapper"><div className='img-equipe-2'></div></div>
                 </div>
-
+                <div class="overlap-11">
+                    <div className="text-wrapper-15">Luciana</div>
+                    <div className="mask-group-wrapper"><div className='img-equipe-3'></div></div>
+                </div>
+                <div className="overlap-12">
+                    <div className="text-wrapper-16">Regina</div>
+                    <div className="mask-group-wrapper"><div className='img-equipe-4'></div></div>
+                </div>
+                <div class="overlap-14">
+                    <div className="text-wrapper-17">Maria</div>
+                    <div className="mask-group-wrapper"><div className='img-equipe-5'></div></div>
+                </div>
+                <div class="overlap-16">
+                    <div className="text-wrapper-18">Daniel</div>
+                    <div className="overlap-15"><div className='img-equipe-6'></div></div>
+                </div>
+                <div className="team-title">
+                    <div className="overlap-group-3">
+                        <div className="text-wrapper-19">Equipe</div>
+                        {/* <img className="pata-equipe" src="img/pata-equipe.png" /> */}
+                    </div>
+                </div>
             </div>
         </section>
 
